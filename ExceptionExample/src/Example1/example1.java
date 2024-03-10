92d6297d1e8803f0de3edc5d77555c5c3a089086
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2024-03-10 23:02:08 +0600

    Exception Excercises

---
+++ ExceptionExample/src/Example1/example1.java
@@ -0,0 +1,27 @@
+package Example1;
+
+public class example1 {
+
+	public static void main(String[] args) {
+	
+		int a=50;
+		int j=0;
+		
+		int data;
+		
+		try {
+			data=a/j;
+		}
+		
+		catch(Exception e) {
+			
+			int data2=a/j;
+		}
+		
+		System.out.println("rest of the code");
+		
+		
+
+	}
+
+}
